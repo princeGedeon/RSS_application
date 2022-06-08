94d575a8c15715b0e0001c11ddeb31a1ac170366
princeGedeon <guedjegedeon03@gmail.com>
2022-06-08 08:02:45 +0100

    First commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  String title;
+
+  HomePage({required this.title});
+  HomePageState createState()=>HomePageState();
+}
+
+class HomePageState extends State<HomePage>{
+
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+
+}
