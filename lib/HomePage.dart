ef9c405b43aba2381b05923d0a33d5ed0916a2a5
princeGedeon <guedjegedeon03@gmail.com>
2022-06-08 13:30:00 +0100

    Mise en place du client

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget{
   String title;
@@ -23,7 +24,33 @@ class HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
-  }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Prince Gédéon Yager")
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getFeed,
+        child: Icon(Icons.add),
+        tooltip: "Récupérer",
 
+      ),
+    );
+  }
+void getFeed() async{
+    String urlString="https://www.francebleu.fr/rss/infos.xml";
+    final client=http.Client();
+    final url=Uri.parse(urlString);
+    final clientResponse=await client.get(url);
+    print(clientResponse.body);
+
+
+}
 }
